17a6247903c078123d83deced148b2f173220e8e
Md Azizur Rahman <106616550+AzizurRahman018@users.noreply.github.com>
2024-12-28 12:41:53 +0600

    kjm

--- Ecommerce/LetsShop/settings.py
+++ Ecommerce/LetsShop/settings.py
@@ -124,7 +124,7 @@ STATIC_URL = 'static/'
 MEDIA_URL = 'media/'
 STATICFILES_DIRS = [BASE_DIR / 'static', ]
 MEDIA_ROOT = BASE_DIR / 'media'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = BASE_DIR / 'Ecommerce/static'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
 
